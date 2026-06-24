20eb2e8085773551ab8475526dacdfa7846c35e9
Naksu123 <142221281+Naksu123@users.noreply.github.com>
2026-06-24 14:45:35 +0800

    Delete test_billing.php

--- test_billing.php
+++
@@ -1,8 +0,0 @@
-<?php
-try {
-    $c = new PDO('mysql:host=localhost;dbname=wattipid;charset=utf8mb4', 'root', '');
-    $c->exec("UPDATE users SET move_in_date='2026-05-17', billing_start_date='2026-05-17', billing_end_date='2026-06-17' WHERE id=6");
-    echo "Done";
-} catch(Exception $e) {
-    echo $e->getMessage();
-}
